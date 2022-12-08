3b902dc6289c70a6247499e0b20d72cbe79507fc
Andrew <andrewsasamori@gmail.com>
2022-12-07 21:56:43 -0500

    Removed my API key but might add back to code later using Git Guardian

--- meal-mates/src/Components/YelpAPI.js
+++ meal-mates/src/Components/YelpAPI.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-//inputOpenAt is if the location is open in 30 minutes or not
+// Have to allow cross-control open access
+// Have to open terminal and use the following lines (make sure in open terminal)
+// open -na Google\ Chrome --args --user-data-dir=/tmp/temporary-chrome-profile-dir --disable-web-security
+
+//inputOpenAt is if the location is open in 30 minutes or not; has to use the Unix time code
+//inputPrice is an integer array, with options of being: 1,2,3,4; 1 is cheapest, 4 is Gordon Ramsay
 export function getRestInformation(inputLocation, inputPrice, inputOpenAt) {
     const options = {
         method: 'GET',
@@ -16,17 +21,20 @@ export function getRestInformation(inputLocation, inputPrice, inputOpenAt) {
         headers: {
             accept: 'application/json',
             Authorization:
-                'Bearer l5_sN8Upkix19lLgwHxpAMThs0G2RBKr0rNRPxJ7ZmlOFagJ9EMi_1vv9o0BJNsto0R0hU1enjBhinEDROi6zODQWf_Tdzk2Lhq47-Z1Xz8EP2EEmjws8MyqSp-LY3Yx'
+                'Bearer <insert Yelp Fusion API key here>'
         }
     };
-
-
+    // Want to get name, distance, image_url, categories
+    let ans = []
     axios
         .request(options)
         .then(function (response) {
-            console.log(response.data);
+            let e = response.data.businesses[0];
+            ans.push(e) //is all the businesses of the list of things that you can retrieve
+            //ans.push(e.name, e.distance, e.image_url, e.categories) // to retrieve these specific values
         })
         .catch(function (error) {
             console.error(error);
         });
+    return ans
 }
