cd2b5d33a6c05ac70c26d3d3af85106aac793078
Jake Lee <jakelee@Jakes-MacBook-Pro.local>
2022-12-08 20:40:59 -0500

    added scroll when button is pressed

--- meal-mates/src/App.js
+++ meal-mates/src/App.js
@@ -1,16 +1,32 @@
 import React from 'react'
 
 import { Location, Match} from './containers';
+import { useRef } from 'react';
 
 import './App.css';
 
 const App = () => {
+  const section1Ref = useRef(null);
+  const section2Ref = useRef(null);
+  const section3Ref = useRef(null);
+
+  const handleClick = (ref) => {
+    ref.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+  
   return (
     <div className = "App">
-        <div style={{height: '100vh'}}>
-          <Location />
+      <div ref={section1Ref}>
+          <Location
+            nextPage={ section2Ref }/>
+      </div>
+      <div ref={section2Ref}>
           <Match />
       </div>
+
     </div>
   )
 }
--- meal-mates/src/containers/location/Location.jsx
+++ meal-mates/src/containers/location/Location.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import './location.css'
-const Location = () => {
+const Location = ({ nextPage }) => {
+
+  const [input, setInput] = React.useState('');
+  const [savedInput, setSavedInput] = React.useState('');
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  }
+
+  const handleClick = (ref) => {
+    setSavedInput(input);
+    ref.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
   return (
     <div className="mealmate__location section__padding" id="home">
     <div className="mealmate__location-content">
@@ -8,8 +24,8 @@ const Location = () => {
         <p>Please enter in your address or city</p>
 
       <div className="mealmate__location-content__input">
-        <input type="location" placeholder="Address or City" />
-        <button type="button">Match Me</button>
+        <input type="location" value={input} onChange={handleChange} placeholder="Address or City" />
+        <button type="button" onClick={() => handleClick(nextPage)}>Match Me</button>
       </div> 
     </div>
    
