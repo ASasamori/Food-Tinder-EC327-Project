bdb85b779bfb5b099438d6cbecc813728dd052a2
Andrew <andrewsasamori@gmail.com>
2022-12-09 12:48:57 -0500

    GeoLocation doesn't work, have bearer token for my acc so please don't steal, but running the app and looking into console will show the 20 closest restaurants to Myles.

--- meal-mates/src/App.js
+++ meal-mates/src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {getRestInformation} from "./Components/YelpAPI";
+import {GeoLocation, wait} from "./Components/GeoLocation";
+
+// for matching, iterate through the type of cousine, in the array, and if matching, hold the information of
+// other type of food
 
 function App() {
+  // let myLocation = new GeoLocation()
+  // let long = myLocation.componentDidMount()
+
+  // Dummy function call to get the closest restaurant from you
+  let ans = getRestInformation('610 Beacon Street Boston',[1,2,3,4])
+  console.log(ans)
   return (
     <div className="App">
       <header className="App-header">
@@ -23,3 +34,4 @@ function App() {
 }
 
 export default App;
+
--- meal-mates/src/Components/GeoLocation.js
+++ meal-mates/src/Components/GeoLocation.js
@@ -1,45 +1,30 @@
-
 import React, { Component } from "react";
 
-// Importing geolocated reducer function
-import { geolocated } from "react-geolocated";
+// Takes around 10-15 seconds to make this call
 
-class App extends Component {
-    render() {
+export class GeoLocation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+    // I have no idea how to make this function public...
+    componentDidMount() {
+        let lat, long
+        navigator.geolocation.getCurrentPosition(function(position) {
+            lat = position.coords.latitude
+            long = position.coords.longitude
+        });
+        console.log(lat)
+        console.log(long)
+        return [lat,long] // Are these not integers? Try running again
+    }
 
-        // Check geolocation supported in
-        // browser or not
-        return this.props.isGeolocationAvailable ? (
-
-            // Check location is enable in
-            // browser or not
-            this.props.isGeolocationEnabled ? (
-
-                // Check coordinates of current
-                // location is available or not
-                this.props.coords ? (
-                    <div>
-                        <h1 style={{ color: "green" }}>GeeksForGeeks</h1>
-                        <h3 style={{ color: "red" }}>
-                            Current latitude and longitude of the user is
-                        </h3>
-                        <ul>
-                            <li>latitude - {this.props.coords.latitude}</li>
-                            <li>longitude - {this.props.coords.longitude}</li>
-                        </ul>
-                    </div>
-                ) : (
-                    <h1>Getting the location data</h1>
-                )
-            ) : (
-                <h1>Please enable location on your browser</h1>
-            )
-        ) : (
-            <h1>Please, update your or change the browser </h1>
-        );
+}
+export function wait(ms){
+    let start = new Date().getTime();
+    let end = start;
+    while(end < start + ms) {
+        end = new Date().getTime();
     }
 }
-
-// Binding geolocated() reducer function to
-// App component, while exporting it
-export default geolocated()(App);
